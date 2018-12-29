1c69e327ee9da1a660bf9af409d69a7b15ce208c
gd <785156621@qq.com>
2018-12-29 10:09:32 +0800

    地理位置

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    address: ''
   },
   //事件处理函数
   bindViewTap: function() {
@@ -42,6 +43,7 @@ Page({
         }
       })
     }
+    this.location();
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -49,6 +51,38 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  location: function(){
+    var that = this;
+    //获取位置
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        const speed = res.speed
+        const accuracy = res.accuracy
+        var locationString = res.latitude + "," + res.longitude;
+        wx.request({
+          url: 'https://apis.map.qq.com/ws/geocoder/v1/',
+          data: {
+            "key": "KZUBZ-77Z3J-PQMFC-KYHC4-XGAVT-RYFTV",//注册腾讯地图开放平台中申请，具体方法见下
+            "location": locationString
+          },
+          method: 'Get',
+          success: function (res) {
+            if(res.data.status === 0){
+              that.setData({
+                'address': res.data.result.address
+              });
+              console.log("请求成功")
+              console.log("请求位置：res" + JSON.stringify(res))
+              console.log("请求位置：res.address" + that.data.address)
+            }
+          }
+        })
+      }
     })
   }
 })
